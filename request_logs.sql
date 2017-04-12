4d96f41463acbd490628240c8e6bd19f9c69c07a
Yubin Chen <yubinchen18@gmail.com>
2017-04-13 01:43:37 +0200

    sql update

--- request_logs.sql
+++ request_logs.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:8889
--- Generation Time: Apr 12, 2017 at 11:32 PM
+-- Generation Time: Apr 13, 2017 at 01:41 AM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `paladin`
@@ -30,8 +24,9 @@ CREATE TABLE `request_logs` (
   `id` int(255) NOT NULL,
   `method` varchar(255) NOT NULL,
   `url` varchar(255) NOT NULL,
-  `response_time` float DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=latin1;
+  `response_time` float DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -51,7 +46,4 @@ ALTER TABLE `request_logs`
 -- AUTO_INCREMENT for table `request_logs`
 --
 ALTER TABLE `request_logs`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=81;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=102;
